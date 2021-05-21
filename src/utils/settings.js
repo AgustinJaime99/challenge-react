d5d553edd47c86f37ebceceaf1ecf3b277c3e634
AgustinJaime99 <71357291+AgustinJaime99@users.noreply.github.com>
2021-05-21 05:16:44 -0300

    Update

--- src/utils/settings.js
+++ src/utils/settings.js
@@ -1,3 +1,4 @@
 export const API_KEY = "10215649667594181";
 
-export const API_URL = "https://superheroapi.com/api";
+export const API_URL =
+  "https://cors-anywhere.herokuapp.com/https://superheroapi.com/api";
